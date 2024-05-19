3daa5286edcacf8675f90860c07af9c38c683c95
Zulmi Fuada <zulmi.f48@gmail.com>
2024-05-19 18:30:40 +0700

    merubah sedikit styling pada css dan menambahkan toggle

--- js/script.js
+++ js/script.js
@@ -1,19 +1,55 @@
-function hitung() {
-    const a = parseFloat(document.getElementById('sisi-a').value);
-    const b = parseFloat(document.getElementById('sisi-b').value);
-    const c = parseFloat(document.getElementById('sisi-c').value);
-    const tinggi = parseFloat(document.getElementById('tinggi').value);
+const areaKeliling = document.getElementById("area-keliling");
+const areaLuas = document.getElementById("area-luas");
 
-    if (isNaN(a) || isNaN(b) || isNaN(c) || isNaN(tinggi)) {
-        alert('Masukkan semua nilai sisi dan tinggi dengan benar.');
+function hidden() {
+    areaLuas.classList.add('hidden');
+    areaKeliling.classList.add('hidden');
+}
+
+hidden();
+
+function luas() {
+    const alas = parseFloat(document.getElementById("alas").value);
+    const tinggi = parseFloat(document.getElementById("tinggi").value);
+
+    if (isNaN(alas) || isNaN(tinggi)) {
+        alert("Masukkan semua nilai alas dan tinggi dengan benar!");
+    }
+
+    // Menghitung luas
+    const luas = 0.5 * alas * tinggi;
+    document.getElementById("luas").innerText = `Luas: ${luas}`;
+
+    document.getElementById("alas").value = "";
+    document.getElementById("tinggi").value = "";
+}
+
+function keliling() {
+    const a = parseFloat(document.getElementById("sisi-a").value);
+    const b = parseFloat(document.getElementById("sisi-b").value);
+    const c = parseFloat(document.getElementById("sisi-c").value);
+
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        alert("Masukkan semua nilai sisi benar!");
         return;
     }
 
     // Menghitung keliling
-    const perimeter = a + b + c;
-    document.getElementById('perimeter').innerText = `Keliling: ${perimeter}`;
+    const keliling = a + b + c;
+    document.getElementById("keliling").innerText = `Keliling: ${keliling}`;
 
-    // Menghitung luas
-    const area = 0.5 * a * tinggi;
-    document.getElementById('area').innerText = `Luas: ${area}`;
+    // Reset Value
+    document.getElementById("sisi-a").value = "";
+    document.getElementById("sisi-b").value = "";
+    document.getElementById("sisi-c").value = "";
+}
+
+function menuKeliling() {
+    areaKeliling.classList.toggle("show");
+    areaKeliling.classList.toggle("hidden");
+}
+
+function menuLuas() {
+    areaLuas.classList.toggle("show");
+    areaLuas.classList.toggle("hidden");
 }
